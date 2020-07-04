5d5ca3d21a88cc96890850d08e5f83abd08b4c10
ilenych <ilenych6@gmail.com>
2020-07-04 17:40:56 +0500

    Add PostViewController

---
+++ SocialNetwork/PostViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PostViewController.swift
+//  SocialNetwork
+//
+//  Copyright © 2020 <ASi. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+//    private let  activityIndicator: UIActivityIndicatorView = {
+//         let ai = UIActivityIndicatorView()
+//         ai.startAnimating()
+//         return ai
+//     }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        setupActivityIndicator()
+    }
+    
+//    func setupActivityIndicator() {
+//        view.addSubview(activityIndicator)
+//        activityIndicator.snp.makeConstraints { (make) in
+//            make.centerX.equalTo(view.snp.centerX)
+//            make.centerY.equalTo(view.snp.centerY)
+//        }
+//    }
+    
+
+}
