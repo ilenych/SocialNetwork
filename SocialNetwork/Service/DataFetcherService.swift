a35e1116b8c59b18e33e8cfa7d30713452727ba7
ilenych <ilenych6@gmail.com>
2020-07-05 22:01:04 +0500

    (DataFetcherService) update

--- SocialNetwork/Service/DataFetcherService.swift
+++ SocialNetwork/Service/DataFetcherService.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 class DataFetcherService {
-    
+    //MARK: - Variables
     static let shared = DataFetcherService()
     private let host = "http://localhost:3000/"
     
@@ -18,10 +18,20 @@ class DataFetcherService {
     init(networkService: DataFetcherProtocol = NetworkService()) {
         self.networkService = networkService
     }
-    
+    //MARK: - Functions
     func fetchGallary(complition: @escaping (GalleryModel?) -> ()) {
         let urlString = host + "gallery/152"
         networkService.fetchData(urlString: urlString, response: complition)
     }
-
+    
+    func fetchPost(complition: @escaping ([PostModel]) -> ()) {
+        let urlString = host + "users/152/wall"
+        networkService.fetchDataArray(urlString: urlString, response: complition)
+    }
+    
+    func fetchLikes(complition: @escaping ([LikesModel]) -> ()) {
+        let urlString = host + "likes"
+        networkService.fetchDataArray(urlString: urlString, response: complition)
+    }
+    
 }
