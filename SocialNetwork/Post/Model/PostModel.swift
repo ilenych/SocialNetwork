671641c5c7ca4ef6bb6d5217db437a0e01bc60bd
ilenych <ilenych6@gmail.com>
2020-07-05 22:03:04 +0500

    Add PostModel

---
+++ SocialNetwork/Post/Model/PostModel.swift
@@ -0,0 +1,21 @@
+//
+//  PostModel.swift
+//  SocialNetwork
+//
+//  Copyright © 2020 <ASi. All rights reserved.
+//
+
+import Foundation
+
+struct PostModel: Decodable {
+    let id: Int
+    let userName: String
+    let userUrlImage: URL
+    let time: String
+    let postText: String
+    let postUrlImage: URL
+    let idUser: Int
+    let userId: Int
+}
+
+
