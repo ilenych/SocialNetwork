308dfa8909bf300ee3a0e5fbe6457534298007ab
ilenych <ilenych6@gmail.com>
2020-07-05 22:00:43 +0500

    (NetworkService) update

--- SocialNetwork/Service/NetworkService.swift
+++ SocialNetwork/Service/NetworkService.swift
@@ -10,12 +10,14 @@ import UIKit
 
 protocol DataFetcherProtocol {
     func fetchData<T: Decodable>(urlString: String, response: @escaping (T?) -> ())
+    func fetchDataArray<T: Decodable>(urlString: String, response: @escaping ([T]) -> ())
 }
 
 class NetworkService: DataFetcherProtocol {
     
     static let shared = NetworkService()
     
+    /// Fetch data
     func fetchData<T: Decodable>(urlString: String, response: @escaping (T?) -> ()) {
         guard let url = URL(string: urlString) else { return }
         let data = try? Data(contentsOf: url)
@@ -25,6 +27,7 @@ class NetworkService: DataFetcherProtocol {
     
     func decodeJSON<T: Decodable>(type: T.Type, data: Data?) -> T? {
         let decoder = JSONDecoder()
+        print(type)
         guard let data = data else { return nil }
         do {
             let objects = try decoder.decode(type.self, from: data)
@@ -35,10 +38,24 @@ class NetworkService: DataFetcherProtocol {
         }
     }
     
-    func downloadImage(urlString: String, completion: @escaping (_ image: UIImage)->()) {
+    /// Fetch data  if return json araay
+    func fetchDataArray<T: Decodable>(urlString: String, response: @escaping ([T]) -> ()) {
+        guard let url = URL(string: urlString) else { return }
+        guard let data = try? Data(contentsOf: url) else { return }
+        let decoder = JSONDecoder()
+        do {
+            let objects: [T] = try decoder.decode([T].self, from: data)
+            response(objects)
+        } catch let jsonError {
+            print("Failed to decode JSON", jsonError)
+            return
+        }
+        
+    }
+    /// Download  image
+    func downloadImage(url: URL, completion: @escaping (_ image: UIImage)->()) {
         DispatchQueue.main.async {
-            guard let imageUrl = URL(string: urlString) else { return }
-            guard let imageData = try? Data(contentsOf: imageUrl) else { return }
+            guard let imageData = try? Data(contentsOf: url) else { return }
             let image = UIImage(data: imageData)
             if let image = image {
                 completion(image)
