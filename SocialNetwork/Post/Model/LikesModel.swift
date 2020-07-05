f23f8ffb811e8bfb61b0030f1977cdf559524898
ilenych <ilenych6@gmail.com>
2020-07-05 22:02:57 +0500

    Add LikesModel

---
+++ SocialNetwork/Post/Model/LikesModel.swift
@@ -0,0 +1,18 @@
+//
+//  LikesModel.swift
+//  SocialNetwork
+//
+//  Copyright © 2020 <ASi. All rights reserved.
+//
+
+import Foundation
+
+struct LikesModel: Decodable {
+    let modelId: Int
+    let likeFire: Int
+    let likeHeartEyes: Int
+    let likeRocket: Int
+    let likeLike: Int
+    let likeBomb: Int
+    let id : Int
+}
